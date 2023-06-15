3e449a2d7ab7da59733699bd0cc5f18108c7cb31
Patryk <nevvgaming@gmail.com>
2023-06-15 17:46:40 +0200

    fix service

--- App/ChatGPT/ChatGPTFactory.go
+++ App/ChatGPT/ChatGPTFactory.go
@@ -1,4 +1,24 @@
 package AppChatGPT
 
+import (
+	"encoding/json"
+
+	DomainModelChatGPT "github.com/ReqqQ/ChatGPT/DomainModel/ChatGPT"
+)
+
+type AppChatGPTFactory struct {
+}
+
 type ChatGPTDTO struct {
+	Json json.RawMessage
+}
+type MessageDTO struct {
+	Content string `json:"content"`
+}
+
+func (c ChatGPTDTO) GetJson() json.RawMessage {
+	return c.Json
+}
+func (factory AppChatGPTFactory) GetMessageDTO(message DomainModelChatGPT.Message) MessageDTO {
+	return MessageDTO{Content: message.GetContent()}
 }
--- App/ChatGPT/ChatGPTService.go
+++ App/ChatGPT/ChatGPTService.go
@@ -1,1 +1,14 @@
 package AppChatGPT
+
+import DomainModelChatGPT "github.com/ReqqQ/ChatGPT/DomainModel/ChatGPT"
+
+type AppChatGPTService struct {
+	DomainChatGPTService DomainModelChatGPT.ChatGPTService
+	AppChatGPTFactory    AppChatGPTFactory
+}
+
+func (gpt AppChatGPTService) GetDataFromChatGPT(dto ChatGPTDTO) MessageDTO {
+	messageVO := gpt.DomainChatGPTService.TranslateIntoHumanLanguage(dto.GetJson())
+
+	return gpt.AppChatGPTFactory.GetMessageDTO(messageVO)
+}
